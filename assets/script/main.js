ca61c3c7b93294061732957b6782c8a31afe22a3
kita147 <work.kitayama@gmail.com>
2025-02-16 14:29:45 +0900

    スライドの2番目を初めにアクティブにする。

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,24 +3,27 @@
 ////////// swiperの設定 //////////
 const swiper = new Swiper(".swiper", {
 
+    // 無限ループさせる
+    loop: true,
+    loopAdditionalSlides: 1,
+    
+    //始めアクティブスライドを2番目のスライドにする
+    initialSlide: 1,
+
     // スライド数の設定
     slidesPerView: 1,
     spaceBetween: 10,
 
-    //無限ループさせる
-    loop: true,
-    loopAdditionalSlides: 1,
-
-    //スライド数が不足している時でもページネーションを表示する
-    watchOverflow: false,
-
-    // ブレークポイントの設定とPC版のスライド数の設
+    // ブレークポイントの設定とPC版のスライド数の設定
     breakpoints: {
         768: {
             slidesPerView: 3,
             spaceBetween: 10,
         },
     },
+
+    // スライド数が不足している時でもページネーションを表示する
+    watchOverflow: false,
 
     // ページネーションの設定
     pagination: {
@@ -29,3 +32,4 @@ const swiper = new Swiper(".swiper", {
         clickable: true,
     },
 });
+
