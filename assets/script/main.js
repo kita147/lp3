a7274a15a4598ff33ab1e2e514bef928dafebdc5
kita147 <work.kitayama@gmail.com>
2025-02-16 13:48:02 +0900

    お客様の声まで作成

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,21 +2,23 @@
 
 ////////// swiperの設定 //////////
 const swiper = new Swiper(".swiper", {
-    oop: true,
 
     // スライド数の設定
     slidesPerView: 1,
-    spaceBetween: 16,
+    spaceBetween: 10,
 
     //無限ループさせる
     loop: true,
     loopAdditionalSlides: 1,
 
+    //スライド数が不足している時でもページネーションを表示する
+    watchOverflow: false,
+
     // ブレークポイントの設定とPC版のスライド数の設
     breakpoints: {
         768: {
         slidesPerView: 3,
-        spaceBetween: 40,
+        spaceBetween: 10,
         },
     },
 
